40272fb45766bcf2f2529f0fdb2acc34412f1b2e
Thibault Seisel <seisel.thibault@gmail.com>
2022-05-22 12:56:35 +0200

    Configure GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
 import { Module } from "@nestjs/common"
+import { GraphQLModule } from "@nestjs/graphql"
+import { AppResolver } from "./app.resolver"
 
 @Module({
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: "schema.gql",
+      sortSchema: true,
+    }),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
